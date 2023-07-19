02c182d0e6949748c814009a6190f81e73d4c45c
Yang JianXin <995462226@qq.com>
2023-07-19 21:00:26 +0800

    init repo

---
+++ firefly/component/dataset.py
@@ -0,0 +1,23 @@
+import json
+from loguru import logger
+from torch.utils.data import Dataset
+
+
+class PretrainDataset(Dataset):
+    def __init__(self, file, tokenizer, max_seq_length):
+        self.tokenizer = tokenizer
+        self.bos_token_id = tokenizer.bos_token_id
+        self.eos_token_id = tokenizer.eos_token_id
+        self.max_seq_length = max_seq_length
+        logger.info('Loading data: {}'.format(file))
+        with open(file, 'r', encoding='utf8') as f:
+            data_list = f.readlines()
+        logger.info("there are {} data in dataset".format(len(data_list)))
+        self.data_list = data_list
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, index):
+        # todo
+        pass
