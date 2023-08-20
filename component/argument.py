a295334476fdc40e1008a88ce9e982ee5fae3288
Yang JianXin <995462226@qq.com>
2023-08-20 11:59:23 +0800

    update QLoRAArguments

--- component/argument.py
+++ component/argument.py
@@ -9,10 +9,11 @@ class QLoRAArguments:
     """
     max_seq_length: int = field(metadata={"help": "输入最大长度"})
     min_seq_length: int = field(metadata={"help": "输入最小长度"})
+    window_step_size: int = field(metadata={"help": "滑动窗口大小"})
     data_path: str = field(metadata={"help": "训练数据路径"})
     model_name_or_path: str = field(metadata={"help": "预训练权重路径"})
 
-    eval_size: int = field(default=1000, metadata={"help": "验证集大小"})
+    eval_size: int = field(default=0, metadata={"help": "验证集大小"})
     tokenizer_name_or_path: str = field(default=None, metadata={"help": "tokenizer路径"})
     train_embedding: bool = field(default=False, metadata={"help": "词表权重是否参与训练"})
     lora_rank: Optional[int] = field(default=64, metadata={"help": "lora rank"})
